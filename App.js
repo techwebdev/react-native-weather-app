327b24115a2b46c3aa9286c4a43a1a15ea6ff168
techwebdev <amit.kadivar3@gmail.com>
2021-06-22 21:09:20 +0530

    work on changes style

--- App.js
+++ App.js
@@ -34,6 +34,7 @@ const weatherIcon = weatherType => {
     return <CloudyIcon width={34} height={34} fill="#fff" />;
   }
 };
+
 const App = () => {
   const {width: windowWidth, height: windowHeight} = useWindowDimensions();
   const scrollX = useRef(new Animated.Value(0)).current;
@@ -72,17 +73,8 @@ const App = () => {
             <View
               style={{width: windowWidth, height: windowHeight}}
               key={index}>
-              <ImageBackground
-                source={bgImg}
-                style={{
-                  flex: 1,
-                }}>
-                <View
-                  style={{
-                    flex: 1,
-                    backgroundColor: 'rgba(0,0,0,0.3)',
-                    padding: 20,
-                  }}>
+              <ImageBackground source={bgImg} style={styles.flexOne}>
+                <View style={styles.imageContainer}>
                   <View style={styles.topInfoWrapper}>
                     <View>
                       <Text style={styles.city}>{location.city}</Text>
@@ -92,7 +84,7 @@ const App = () => {
                       <Text style={styles.temparature}>
                         {location.temparature}
                       </Text>
-                      <View style={{flexDirection: 'row'}}>
+                      <View style={styles.flexDirectionRow}>
                         {weatherIcon(location.weatherType)}
                         <Text style={styles.weatherType}>
                           {location.weatherType}
@@ -100,64 +92,50 @@ const App = () => {
                       </View>
                     </View>
                   </View>
-                  <View
-                    style={{
-                      borderBottomColor: 'rgba(255,255,255,0.7)',
-                      marginTop: 20,
-                      borderBottomWidth: 1,
-                    }}></View>
+                  <View style={styles.bottomInfoWrapperContainer} />
 
                   <View style={styles.bottomInfoWrapper}>
-                    <View style={{alignItems: 'center'}}>
+                    <View style={[styles.alignItemsCenter]}>
                       <Text style={styles.infoText}>Wind</Text>
-                      <Text style={[styles.infoText, {fontSize: 24}]}>
+                      <Text style={[styles.infoText, styles.fontSize24]}>
                         {location.wind}
                       </Text>
                       <Text style={styles.infoText}>Km/h</Text>
                       <View style={styles.infoBar}>
                         <View
-                          style={{
-                            width: location.wind / 2,
-                            height: 5,
-                            backgroundColor: '#69f0aE',
-                          }}
+                          style={[styles.windStyle, {width: location.wind / 2}]}
                         />
                       </View>
                     </View>
-                    <View style={{alignItems: 'center'}}>
+                    <View style={[styles.alignItemsCenter]}>
                       <Text style={styles.infoText}>Rain</Text>
-                      <Text style={[styles.infoText, {fontSize: 24}]}>
+                      <Text style={[styles.infoText, styles.fontSize24]}>
                         {location.rain}
                       </Text>
                       <Text style={styles.infoText}>%</Text>
                       <View style={styles.infoBar}>
                         <View
-                          style={{
-                            width: location.rain / 2,
-                            height: 5,
-                            backgroundColor: '#F44336',
-                          }}
+                          style={[styles.rainStyle, {width: location.rain / 2}]}
                         />
                       </View>
                     </View>
-                    <View style={{alignItems: 'center'}}>
+                    <View style={[styles.alignItemsCenter]}>
                       <Text style={styles.infoText}>Humidity</Text>
-                      <Text style={[styles.infoText, {fontSize: 24}]}>
+                      <Text style={[styles.infoText, styles.fontSize24]}>
                         {location.humidity}
                       </Text>
                       <Text style={styles.infoText}>%</Text>
                       <View style={styles.infoBar}>
                         <View
-                          style={{
-                            width: location.humidity / 2,
-                            height: 5,
-                            backgroundColor: '#F44336',
-                          }}
+                          style={[
+                            styles.rainStyle,
+                            {width: location.humidity / 2},
+                          ]}
                         />
                       </View>
                     </View>
                   </View>
-                  <Text style={{color: 'white'}}>{location.city}</Text>
+                  <Text style={styles.whiteColor}>{location.city}</Text>
                 </View>
               </ImageBackground>
             </View>
@@ -195,10 +173,26 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
+  whiteColor: {color: 'white'},
+  fontSize24: {fontSize: 24},
+  alignItemsCenter: {
+    alignItems: 'center',
+  },
+  flexDirectionRow: {
+    flexDirection: 'row',
+  },
+  flexOne: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    padding: 20,
   },
   appHeader: {
     position: 'absolute',
@@ -239,10 +233,23 @@ const styles = StyleSheet.create({
     lineHeight: 34,
     marginLeft: 10,
   },
+  bottomInfoWrapperContainer: {
+    borderBottomColor: 'rgba(255,255,255,0.7)',
+    marginTop: 20,
+    borderBottomWidth: 1,
+  },
   bottomInfoWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginVertical: 20,
+  },
+  windStyle: {
+    height: 5,
+    backgroundColor: '#69f0aE',
+  },
+  rainStyle: {
+    height: 5,
+    backgroundColor: '#F44336',
   },
   infoText: {
     color: 'white',
